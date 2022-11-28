13eacc8ac1e91cf08301e709de5f62ad60be969d
Alvaro Mendoza <alvaro_p_m_c@hotmail.com>
2022-11-28 15:34:10 +0100

    Add chagnes

--- Test Suites/test-collection.ts
+++ Test Suites/test-collection.ts
@@ -22,7 +22,7 @@
             <groupName>Web Desktop</groupName>
             <profileName>default</profileName>
             <requireConfigurationData>false</requireConfigurationData>
-            <runConfigurationId>Chrome (headless)</runConfigurationId>
+            <runConfigurationId>Chrome</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/test-suite-load-landings</testSuiteEntity>
